91edb2a69df69c3536ed87dfd9235cdd2a6088ba
Jacek <jacgulij@gmail.com>
2019-04-13 17:56:21 +0200

    INIT

--- main.cpp
+++ main.cpp
@@ -1,89 +1,40 @@
-#include <windows.h>
-#include <vector>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
-typedef vector<HWND> row;
+using namespace sf;
 
-HWND hPrzycisk;
-vector<row> hText;
+int main()
+{
+    // Create the main window
+    RenderWindow app(VideoMode(800, 600), "SFML window");
 
-/* This is where all the input to the window goes to */
-LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
-	switch(Message) {
-		
-		/* Upon destruction, tell the main thread to stop */
-		case WM_DESTROY: {
-			PostQuitMessage(0);
-			break;
-		}
-		
-		/* All other messages (a lot of them) are processed using default procedures */
-		default:
-			return DefWindowProc(hwnd, Message, wParam, lParam);
-	}
-	return 0;
-}
+    // Load a sprite to display
+    Texture texture;
+    if (!texture.loadFromFile("cb.bmp"))
+        return EXIT_FAILURE;
+    Sprite sprite(texture);
 
-/* The 'main' function of Win32 GUI programs: this is where execution starts */
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	WNDCLASSEX wc; /* A properties struct of our window */
-	HWND hwnd; /* A 'HANDLE', hence the H, or a pointer to our window */
-	MSG msg; /* A temporary location for all messages */
+	// Start the game loop
+    while (app.isOpen())
+    {
+        // Process events
+        Event event;
+        while (app.pollEvent(event))
+        {
+            // Close window : exit
+            if (event.type == Event::Closed)
+                app.close();
+        }
 
-	/* zero out the struct and set the stuff we want to modify */
-	memset(&wc,0,sizeof(wc));
-	wc.cbSize		 = sizeof(WNDCLASSEX);
-	wc.lpfnWndProc	 = WndProc; /* This is where we will send messages to */
-	wc.hInstance	 = hInstance;
-	wc.hCursor		 = LoadCursor(NULL, IDC_ARROW);
-	
-	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-	wc.lpszClassName = "WindowClass";
-	wc.hIcon		 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */
-	wc.hIconSm		 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */
+        // Clear screen
+        app.clear();
 
-	if(!RegisterClassEx(&wc)) {
-		MessageBox(NULL, "Window Registration Failed!","Error!",MB_ICONEXCLAMATION|MB_OK);
-		return 0;
-	}
+        // Draw the sprite
+        app.draw(sprite);
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Gauss-Jordan",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, /* x */
-		CW_USEDEFAULT, /* y */
-		640, /* width */
-		480, /* height */
-		NULL,NULL,hInstance,NULL);
+        // Update the window
+        app.display();
+    }
 
-	if(hwnd == NULL) {
-		MessageBox(NULL, "Window Creation Failed!","Error!",MB_ICONEXCLAMATION|MB_OK);
-		return 0;
-	}
-	
-	hPrzycisk = CreateWindowEx( 0, "BUTTON", "Calculate", WS_CHILD | WS_VISIBLE,
-	10, 5, 150, 30, hwnd, NULL, hInstance, NULL );
-
-	int size = 2;
-
-	for(int y=0;y<size;y++){
-		row r;
-		for(int x=0;x<size+1;x++){
-			HWND textBox = CreateWindowEx( 0, "EDIT", NULL, WS_CHILD | WS_VISIBLE | WS_BORDER,
-			10 + 110 * x, 45 + 25 * y, 105, 20, hwnd, NULL, hInstance, NULL );
-			
-			r.push_back(textBox);
-		}
-		hText.push_back(r);
-	}
-	
-	/*
-		This is the heart of our program where all input is processed and 
-		sent to WndProc. Note that GetMessage blocks code flow until it receives something, so
-		this loop will not produce unreasonably high CPU usage
-	*/
-	while(GetMessage(&msg, NULL, 0, 0) > 0) { /* If no error is received... */
-		TranslateMessage(&msg); /* Translate key codes to chars if present */
-		DispatchMessage(&msg); /* Send it to WndProc */
-	}
-	return msg.wParam;
+    return EXIT_SUCCESS;
 }
